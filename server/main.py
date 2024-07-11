2a3e0b28bfbda48154db2c7fe2f7498da2bb5ad3
hugofolloni <hugofollogua07@gmail.com>
2024-07-11 15:58:34 -0300

    Verifying database size to decide autoinsert

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from database import retrieve_songs, add_songs, TrackInfo
+from database import retrieve_songs, add_songs, get_database_size, TrackInfo
 from analysis import apply_pca, find_similarity, get_playlist_info
 from local import print_data
 
@@ -6,12 +6,13 @@ from local import print_data
 def find_songs(playlist: str, amount:int, adding:bool=False) -> list[TrackInfo]:
     user_infos, user_ids, user_songs = get_playlist_info(playlist)
     database_infos = retrieve_songs(filters=f"WHERE track_id NOT IN {user_ids}")
-    if adding:
-        add_songs(user_infos, user_songs) ## Add the playlist's songs to our database (slows down the process, but refines it)
+    if adding or get_database_size() < 4000:
+        add_songs(user_infos, user_songs) ## Add playlist's songs to our database (slows down the process, but refines it)
     user_vector = apply_pca(user_infos)
 
     return find_similarity(user_vector, database_infos, amount)
 
+## Local search always add playlist's songs to our database
 if __name__ == "__main__":
     playlist = input("Playlist: ")
     amount = int(input("Amount: "))
